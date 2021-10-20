d1e4e29ba3fe524996b730dd4666169f948707a4
MohamedOmar2020 <mohamed222ahmed222@gmail.com>
2021-10-20 16:07:54 -0400

    add deepslide pip

--- clam/03_PrepMetaData_train_pten.py
+++ clam/03_PrepMetaData_train_pten.py
@@ -32,7 +32,7 @@ pten['case_id'] =  [i.replace('.', '-') for i in pten['case_id']]
 
 #################################
 ## Get the IDs for the WSIs and patients
-slide_dir = '/athena/marchionnilab/scratch/lab_data/Mohamed/pca_outcome/data/tiles_clam_256/patches'
+slide_dir = '/athena/marchionnilab/scratch/lab_data/Mohamed/pca_outcome/data/tiles_clam_512/patches'
 slides = listdir(slide_dir)
 
 # make a dataframe: slide_id: slide ID! // case_id: patient ID
