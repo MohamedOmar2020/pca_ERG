bb35da8e565dd4786a7d3558979b0640134bade0
MohamedOmar2020 <mohamed222ahmed222@gmail.com>
2022-03-22 10:57:32 -0400

    heatmaps

--- clam/02_sub_featExtraction_NatHist.sh
+++ clam/02_sub_featExtraction_NatHist.sh
@@ -12,5 +12,5 @@ source ~/.bashrc
 
 conda activate /home/mao4005/.conda/envs/clam
 
-CUDA_VISIBLE_DEVICES=0,1 python clam/extract_features_fp.py --data_h5_dir data/tiles_clam_512_NatHist --data_slide_dir data/NatHistory --csv_path data/tiles_clam_512_NatHist/process_list_autogen_featExt.csv --feat_dir data/features_clam_512_NatHist_patchSize1 --batch_size 512 --slide_ext .ndpi --target_patch_size 1
+CUDA_VISIBLE_DEVICES=0,1 python clam/extract_features_fp.py --data_h5_dir data/tiles_clam_256_NatHist_lvl1 --data_slide_dir data/NatHistory --csv_path data/tiles_clam_256_NatHist_lvl1/process_list_autogen_featExt.csv --feat_dir data/features_clam_256_NatHist_lvl1 --batch_size 512 --slide_ext .ndpi
 
--- clam/05_sub_training_ERG_NatHist.sh
+++ clam/05_sub_training_ERG_NatHist.sh
@@ -12,4 +12,4 @@ source ~/.bashrc
 
 conda activate /home/mao4005/.conda/envs/clam
 
-CUDA_VISIBLE_DEVICES=0,1 python clam/main.py --early_stopping --drop_out --lr 1e-4 --k 10 --label_frac 0.7 --exp_code pca_ERG_512_NatHist_MBbig_B32 --weighted_sample --bag_loss ce --inst_loss svm --task pca_ERG_NatHist --model_type clam_mb --model_size big --B 32 --log_data --data_root_dir data/features_clam_512_NatHist_lvl1 --results_dir objs/clam/training_results/NatHist --max_epochs 100 --split_dir pca_ERG_NatHist_70
+CUDA_VISIBLE_DEVICES=0,1 python clam/main.py --early_stopping --drop_out --lr 1e-4 --k 10 --label_frac 1.0 --exp_code pca_ERG_256_NatHist_MBbig_B16CE --weighted_sample --bag_loss ce --inst_loss ce --task pca_ERG_NatHist --model_type clam_mb --model_size big --B 16 --log_data --data_root_dir data/features_clam_256_NatHist_lvl1 --results_dir objs/clam/training_results/NatHist --max_epochs 100 --split_dir pca_ERG_NatHist_70
