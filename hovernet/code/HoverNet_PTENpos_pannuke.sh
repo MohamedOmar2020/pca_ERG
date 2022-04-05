6622b443b63862efa52d3ed4c6e1bac9d4c5b966
MohamedOmar2020 <mohamed222ahmed222@gmail.com>
2022-04-05 17:53:26 -0400

    minor

--- hovernet/code/HoverNet_PTENpos_pannuke.sh
+++ hovernet/code/HoverNet_PTENpos_pannuke.sh
@@ -9,8 +9,8 @@
 #SBATCH --gres=gpu:2
 
 source ~/.bashrc
-conda activate /home/mao4005/.conda/envs/hovernet
-
+#conda activate /home/mao4005/.conda/envs/hovernet
+conda activate /athena/marchionnilab/scratch/lab_data/Mohamed/pca_outcome/hovernet_env
 python hovernet/code/hover_net/run_infer.py --nr_types=6 --model_path=hovernet/models/hovernet_fast_pannuke_type_tf2pytorch.tar --type_info_path hovernet/code/hover_net/type_info_pannuke.json --model_mode=fast tile --input_dir hovernet/input/pten/pos --output_dir hovernet/output/pten/pos/pannuke
 
 
